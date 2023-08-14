3b3d58d5758269782b4d3457e54fd3195ed111b7
Sarwar Hossain <sarwarhridoy4@gmail.com>
2023-08-14 21:22:48 +0600

    install react-bootstrap bootstrap

--- src/components/RequestLink/Alert/Alert.tsx
+++ src/components/RequestLink/Alert/Alert.tsx
@@ -5,7 +5,7 @@ const Alert = () => {
     <div className='flex items-center justify-start gap-3 bg-[#eef2fe] p-[10px]'>
       <img src={alert} alt="alert" />
       <p>
-        our wallet is connected to <b>Ethereum Kovan</b> , so you are requesting
+        Your wallet is connected to <b>Ethereum Kovan</b> , so you are requesting
         <b>Ethereum Kovan</b> Link/ETH.
       </p>
     </div>
--- src/components/RequestLink/RequestLink.tsx
+++ src/components/RequestLink/RequestLink.tsx
@@ -6,7 +6,7 @@ const RequestLink = () => {
   return (
     <div className='bg-[#eef2fe]'>
       <div className="py-12">
-      <div className="container mx-auto px-[30px]">
+      <div className="px-[30px]">
       <h2 className="text-[#9b1fe9] text-[35px] font-semibold mb-5">Request testnet LINK</h2>
       <p className="text-[16px] text-[#7177a5]">
         Get testnet LINK for an account on one of the supported blockchain
