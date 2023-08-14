1b565bdda537d4fdb9a00aa096922fdf928aecb2
Sarwar Hossain <sarwarhridoy4@gmail.com>
2023-08-14 22:34:16 +0600

    footer done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,8 @@
-
+import './Footer.css'
 const Footer = () => {
   return (
-      <footer className="w-full font-varela flex items-center justify-center">
-          <span className="text-[13px] font-normal pt-6 pb-6">&copy; copyright 2022 - All Right Reserved by Faucet</span>
+      <footer className="footer">
+          <span>&copy; copyright 2022 - All Right Reserved by Faucet</span>
     </footer>
   )
 }
