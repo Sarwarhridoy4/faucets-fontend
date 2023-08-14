603c460bd1c066f75465e296bd93291d3adf8116
Sarwar Hossain <sarwarhridoy4@gmail.com>
2023-08-14 23:32:45 +0600

    alert done

--- src/components/RequestLink/Alert/Alert.tsx
+++ src/components/RequestLink/Alert/Alert.tsx
@@ -1,13 +1,15 @@
 import alert from '../../../assets/alert.svg'
-
+import "./Alert.css"
 const Alert = () => {
   return (
-    <div>
+    <div className='form'>
+      <div className="warning d-flex align-align-items-center gap-2">
       <img src={alert} alt="alert" />
       <p>
         Your wallet is connected to <b>Ethereum Kovan</b> , so you are requesting
         <b>Ethereum Kovan</b> Link/ETH.
       </p>
+      </div>
     </div>
   );
 };
--- src/components/RequestLink/RequestLink.tsx
+++ src/components/RequestLink/RequestLink.tsx
@@ -1,16 +1,16 @@
 
 import RequestHistory from "./RequestHistory/RequestHistory";
 import SubmitForm from "./SubmitForm/SubmitForm";
-
+import "./RequestLink.css"
 const RequestLink = () => {
   return (
-    <div>
-      <div>
-      <div>
+    <div className="container">
+      <div className="main_content">
+      <div className="title">
       <h2>Request testnet LINK</h2>
       <p>
         Get testnet LINK for an account on one of the supported blockchain
-        testnets so you can <br /> create and test your own oracle and Chainlinked
+        testnets so you can create and test your own oracle and Chainlinked
         smart contract
       </p>
       </div>
