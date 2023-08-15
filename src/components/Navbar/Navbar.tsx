b012ae655a8c547feee1107e01670e5cd11c4741
Sarwar Hossain <sarwarhridoy4@gmail.com>
2023-08-15 09:59:08 +0600

    navbar complete

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,48 @@
-
-
+import "./Navbar.css";
+import faucets from "../../assets/faucets.webp";
+import dropdown from "../../assets/dropdown.svg";
+import user from "../../assets/user.svg";
+import wallet from "../../assets/wallet.svg";
 const Navigation = () => {
   return (
-    <nav>
-      <h2>Faucets</h2>
+    <nav className='header bg-white'>
+      <div className='container'>
+        <div className='header_Container'>
+          <div className='logo'>
+            <a href='/'>
+              <h2>Faucets</h2>
+            </a>
+          </div>
+          <div className='right-content'>
+            <div className='networks'>
+              <div className='networkList'>
+                <div className='network_State d-flex align-items-center justify-content-center'>
+                  <p className="d-flex align-items-center justify-content-center">
+                    <img src={faucets} alt='faucets' className='networksImg m-0' />
+                    <span id='smallText' className="d-none d-lg-inline">Ethereum Kovan</span>
+                  </p>
+                  <span>
+                    <img src={dropdown} alt='drop-icon' />
+                  </span>
+                </div>
+              </div>
+              <div className='connect_wallet'>
+                <button className='connectButton'>
+                  <img src={wallet} alt='wallet' className="me-1" />
+                  <span className='btn-text d-none d-lg-block'>Connect Wallet</span>
+                </button>
+              </div>
+              <div className="user">
+                <div className="icon">
+                  <img src={user} alt="user"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
